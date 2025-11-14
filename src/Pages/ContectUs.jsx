a2fd2e04310d34e52a6894b404d0d722a8874199
Shailav <justashailav02@gmail.com>
2025-11-14 15:09:16 +0530

    updated frontend again

--- src/Pages/ContectUs.jsx
+++ src/Pages/ContectUs.jsx
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
-import heroImage from "../assets/hero-image.png";
+
 import logo from "../assets/logo-himalayas.png";
 import { FaInstagram, FaFacebook } from "react-icons/fa";
 import { contactFormHandler } from "@/store/slices/contactSlice";
