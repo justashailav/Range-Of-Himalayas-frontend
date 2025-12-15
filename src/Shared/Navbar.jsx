f8cd8c4ce22810383eaa442c36d85dc94acaa6fe
Shailav <justashailav02@gmail.com>
2025-12-15 18:28:39 +0530

    updated b  again

--- src/Shared/Navbar.jsx
+++ src/Shared/Navbar.jsx
@@ -62,7 +62,8 @@ export default function Navbar() {
 
           <div className="flex items-center gap-4 sm:gap-4 mt-2">
             <Sheet open={openCart} onOpenChange={setOpenCart}>
-              <Link to="/wishlist">
+            <div className="relative">
+               <Link to="/wishlist">
                 <Heart color="white" className="cursor-pointer text-2xl" />
                 {wishListCount > 0 && (
                   <span className="absolute -top-2 -right-2 bg-red-600 text-white text-xs font-bold rounded-full px-1.5 py-0.5">
@@ -70,6 +71,8 @@ export default function Navbar() {
                   </span>
                 )}
               </Link>
+            </div>
+
               <div className="relative">
                 <FaShoppingCart
                   onClick={() => setOpenCart(true)}
