1200bc606f90b291a8315533d6abbe8f05aab7a2
Shailav <justashailav02@gmail.com>
2025-12-15 17:00:22 +0530

    updated b  again

--- src/Pages/OrderTracking.jsx
+++ src/Pages/OrderTracking.jsx
@@ -6,8 +6,9 @@ import { motion } from "framer-motion";
 import { CheckCircle, Package, Truck, Home, Clock, Search } from "lucide-react";
 import { Helmet } from "react-helmet";
 
-const SOCKET_URL = "http://localhost:3000";
-const API_BASE_URL = "http://localhost:3000/api/v1/order";
+const SOCKET_URL = import.meta.env.VITE_SOCKET_URL;
+const API_BASE_URL = `${import.meta.env.VITE_API_BASE_URL}/api/v1/order`;
+
 const socket = io(SOCKET_URL, { withCredentials: true });
 
 const ORDER_STAGES = [
