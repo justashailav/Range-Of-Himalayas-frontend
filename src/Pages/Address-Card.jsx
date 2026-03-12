8a6096338ca6a6474ae9fceecda114e8d796494f
Shailav <justashailav02@gmail.com>
2026-03-12 22:37:53 +0530

    Initial frontend commit

--- src/Pages/Address-Card.jsx
+++ src/Pages/Address-Card.jsx
@@ -40,6 +40,7 @@
 import { Button } from "@/components/ui/button";
 import { Card, CardContent, CardFooter } from "@/components/ui/card";
 import { Label } from "@/components/ui/label";
+import { Check, Phone } from "lucide-react";
 
 export default function AddressCard({
   addressInfo,
