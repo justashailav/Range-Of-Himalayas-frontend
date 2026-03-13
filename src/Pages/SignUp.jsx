dd6dbfa66ee8f28a0b859b037eaa781b859c656d
Shailav <justashailav02@gmail.com>
2026-03-13 15:35:06 +0530

    Initial frontend commit

--- src/Pages/SignUp.jsx
+++ src/Pages/SignUp.jsx
@@ -5,6 +5,7 @@ import { Link, Navigate, useNavigate } from "react-router-dom";
 import { toast } from "react-toastify";
 import logo from "../assets/logo-himalayas.png"
 import { Helmet } from "react-helmet";
+import { motion, AnimatePresence } from "framer-motion";
 export default function SignUp() {
   const [name, setName] = useState("");
   const [email, setEmail] = useState("");
