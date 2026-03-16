0d0ddbaad019f5b3fb47a17fb3214be50e985ece
Shailav <justashailav02@gmail.com>
2026-03-16 12:23:45 +0530

    Initial frontend commit

--- src/components/Checkout.jsx
+++ src/components/Checkout.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect,useRef } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { Button } from "@/components/ui/button";
 import Address from "@/Pages/Address";
