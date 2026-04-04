99d7eb898a3146661d46586768f1d6c5758d8aa5
Shailav <justashailav02@gmail.com>
2026-04-04 22:22:21 +0530

    Initial frontend commit

--- src/Pages/OrderTracking.jsx
+++ src/Pages/OrderTracking.jsx
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from "react";
 import io from "socket.io-client";
 import axios from "axios";
-import { motion } from "framer-motion";
+import {AnimatePresence, motion } from "framer-motion";
 import {
   Package,
   Truck,
