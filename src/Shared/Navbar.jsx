6618a4d37915cc61290b938a09f5cc7078974785
Shailav <justashailav02@gmail.com>
2026-05-02 10:04:20 +0530

    Initial frontend commit

--- src/Shared/Navbar.jsx
+++ src/Shared/Navbar.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { CiSearch } from "react-icons/ci";
 import { Link, useNavigate } from "react-router-dom";
 import logo from "../assets/logo-himalayas.png";
